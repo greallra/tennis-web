46ce78de1daede9e70f5ae7c09747a2ab0e695b9
greallra <greallra@tcd.ie>
2020-09-27 20:45:36 +0100

    added build

---
+++ profiles/build/precache-manifest.33fbac9cd5f20f40f0cfc20140954686.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "973de9df442483789fc5ea198b3202df",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1bbac66811a6fc1f794b",
+    "url": "/static/css/main.8ab9a205.chunk.css"
+  },
+  {
+    "revision": "d3292cbc6bf42143b9f4",
+    "url": "/static/js/2.9f3d91d0.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.9f3d91d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1bbac66811a6fc1f794b",
+    "url": "/static/js/main.4d82db4c.chunk.js"
+  },
+  {
+    "revision": "56a3ce93faef4b94b21e",
+    "url": "/static/js/runtime-main.1342181d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
