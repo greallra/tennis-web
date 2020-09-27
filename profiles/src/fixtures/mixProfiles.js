6a4771a2c7ecbe5547b16ec7020426d4d3687ee5
greallra <greallra@tcd.ie>
2020-09-27 19:08:22 +0100

    removed profiles

--- profiles/src/fixtures/mixProfiles.js
+++
@@ -1,7 +0,0 @@
-import playerRankings from './playerRankings.json';
-
-const men = playerRankings.rankings[1].player_rankings;
-const women = playerRankings.rankings[0].player_rankings;
-const mix = [...men, ...women]
-
-export default mix;
